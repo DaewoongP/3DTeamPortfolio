665b598b1aafc6c306a32f84cd774585da623fc7
duoface88 <72694443+duoface88@users.noreply.github.com>
2023-08-30 16:16:48 +0900

    QuadTree 적용
    대인이 형의 QuadTree 코드 참고
    
    60프레임 방어 된다.

--- Tool/Private/Object_Window.cpp
+++ Tool/Private/Object_Window.cpp
@@ -26,17 +26,9 @@ void CObject_Window::Tick(_float fTimeDelta)
 {
 	__super::Tick(fTimeDelta);
 
-	// 한번만 QUADTERRAIN 값들 초기화
-	if (true == m_isInitQuadTerrain)
-	{
-		InitializeQuadTerrain();
-		m_isInitQuadTerrain = false;
-	}
-
 	ImGui::Begin("Object", nullptr, m_WindowFlag);
 
 	_float3 vPos = Find_PickingPos();
-	//_float3 vPos = Find_PickingPosTest();
 
 	// 현재 피킹 위치 표시
 	ImGui::Text("Picking Position");
@@ -71,154 +63,22 @@ _float3 CObject_Window::Find_PickingPos()
 		static_cast<CTerrain*>(pGameInstance->Find_GameObject_In_Layer(LEVEL_TOOL, TEXT("Layer_Tool"),
 			TEXT("GameObject_Terrain")))->Get_Buffer()); ENDINSTANCE;
 
-	_float fDist = FLT_MAX;
-	m_fDist = FLT_MAX;
+	float fDist = FLT_MAX; // 피킹 연산 후 최종 거리값
 
-	_uint ivtxcntX = pTerrain->Get_TerrainSizeX();
-	_uint ivtxcntZ = pTerrain->Get_TerrainSizeZ();
+	_bool bResult = pTerrain->IsPicked(vRayPos, vRayDir, fDist);
 
-	const _float3* pTerrainVtxPos = pTerrain->Get_PosArray();
-
-	for (_ulong j = 0; j < ivtxcntZ - 1; ++j)
+	if (true == bResult)
 	{
-		for (_ulong i = 0; i < ivtxcntX - 1; ++i)
+		if (FLT_MAX > fDist)
 		{
-			_ulong dwIndex = i * ivtxcntX + j;
+			_float4 vFinalPos;
 
-			_ulong		iIndices[4] = {
-				dwIndex + ivtxcntX,
-				dwIndex + ivtxcntX + 1,
-				dwIndex + 1,
-				dwIndex
-			};
+			vRayDir *= fDist;
+			vFinalPos = vRayPos + vRayDir;
 
-			if (TriangleTests::Intersects(vRayPos, vRayDir,
-				pTerrainVtxPos[iIndices[0]], pTerrainVtxPos[iIndices[1]], pTerrainVtxPos[iIndices[2]],
-				fDist))
-			{
-				if (m_fDist > fDist)
-					m_fDist = fDist;
-			}
-
-			if (TriangleTests::Intersects(vRayPos, vRayDir,
-				pTerrainVtxPos[iIndices[0]], pTerrainVtxPos[iIndices[2]], pTerrainVtxPos[iIndices[3]],
-				fDist))
-			{
-				if (m_fDist > fDist)
-					m_fDist = fDist;
-			}
+			return _float3(vFinalPos.x, vFinalPos.y, vFinalPos.z);
 		}
-	}
-
-	if (FLT_MAX > m_fDist)
-	{
-		_float4 vFinalPos;
-
-		vRayDir *= m_fDist;
-		vFinalPos = vRayPos + vRayDir;
-
-		return _float3(vFinalPos.x, vFinalPos.y, vFinalPos.z);
-	}
-
-	return _float3(-1.f, -1.f, -1.f);
-}
-
-_float3 CObject_Window::Find_PickingPosTest()
-{
-	_float4 vRayPos = { 0.f, 0.f, 0.f, 1.f };
-	_float4 vRayDir = { 0.f, 0.f, 0.f, 0.f };
-
-	BEGININSTANCE; pGameInstance->Get_WorldMouseRay(m_pContext, g_hWnd, &vRayPos, &vRayDir);
-
-	CVIBuffer_Terrain* pTerrain = static_cast<CVIBuffer_Terrain*>(
-		static_cast<CTerrain*>(pGameInstance->Find_GameObject_In_Layer(LEVEL_TOOL, TEXT("Layer_Tool"),
-			TEXT("GameObject_Terrain")))->Get_Buffer()); ENDINSTANCE;
-
-	_float fDist = FLT_MAX;
-	m_fDist = FLT_MAX;
-
-	_uint ivtxcntX = pTerrain->Get_TerrainSizeX() - 1;
-	_uint ivtxcntZ = pTerrain->Get_TerrainSizeZ() - 1;
-
-	const _float3* pTerrainVtxPos = pTerrain->Get_PosArray();
-
-	for (_ulong j = m_TerrainZDesc.iStart; j < m_TerrainZDesc.iEnd; ++j)
-	{
-		for (_ulong i = m_TerrainXDesc.iStart; i < m_TerrainXDesc.iEnd; ++i)
-		{
-			_ulong dwIndex = i * (m_TerrainXDesc.iEnd + 1) + j;
-
-			_ulong		iIndices[4] = {
-				dwIndex + m_TerrainXDesc.iEnd + 1,
-				dwIndex + m_TerrainXDesc.iEnd + 2,
-				dwIndex + 1,
-				dwIndex
-			};
-
-			if (TriangleTests::Intersects(vRayPos, vRayDir,
-				pTerrainVtxPos[iIndices[0]], pTerrainVtxPos[iIndices[1]], pTerrainVtxPos[iIndices[2]],
-				fDist))
-			{
-				if (m_fDist > fDist)
-					m_fDist = fDist;
-			}
-
-			if (TriangleTests::Intersects(vRayPos, vRayDir,
-				pTerrainVtxPos[iIndices[0]], pTerrainVtxPos[iIndices[2]], pTerrainVtxPos[iIndices[3]],
-				fDist))
-			{
-				if (m_fDist > fDist)
-					m_fDist = fDist;
-			}
-		}
-	}
-
-	if (FLT_MAX > m_fDist)
-	{
-		_float4 vFinalPos;
-
-		vRayDir *= m_fDist;
-		vFinalPos = vRayPos + vRayDir;
-
-		// 이전 결과에 따라 다음 순환할 범위를 정해줌
-		// X : 0 ~ iCenter, Z : 0 ~ iCenter
-		if (m_TerrainXDesc.iCenter >= vFinalPos.x && m_TerrainZDesc.iCenter >= vFinalPos.z)
-		{
-			m_TerrainXDesc.iStart = m_TerrainXDesc.iOriginStart;
-			m_TerrainXDesc.iEnd = m_TerrainXDesc.iCenter;
-			m_TerrainZDesc.iStart = m_TerrainZDesc.iOriginStart;
-			m_TerrainZDesc.iEnd = m_TerrainZDesc.iCenter;
-		}
-
-		// X : iCenter ~ ivtxcnt, Z : 0 ~ iCenter
-		else if (m_TerrainZDesc.iCenter < vFinalPos.z && m_TerrainZDesc.iCenter >= vFinalPos.z)
-		{
-			m_TerrainXDesc.iStart = m_TerrainXDesc.iCenter;
-			m_TerrainXDesc.iEnd = m_TerrainXDesc.iEnd;
-			m_TerrainZDesc.iStart = m_TerrainZDesc.iOriginStart;
-			m_TerrainZDesc.iEnd = m_TerrainZDesc.iCenter;
-		}
-
-		// X : 0 ~ iCenter, Z : iCenter ~ ivtzcnt
-		else if (m_TerrainXDesc.iCenter >= vFinalPos.x && m_TerrainZDesc.iCenter < vFinalPos.z)
-		{
-			m_TerrainXDesc.iStart = m_TerrainXDesc.iOriginStart;
-			m_TerrainXDesc.iEnd = m_TerrainXDesc.iCenter;
-			m_TerrainZDesc.iStart = m_TerrainZDesc.iCenter;
-			m_TerrainZDesc.iEnd = m_TerrainZDesc.iEnd;;
-		}
-
-		// X : iCenter ~ ivtxcnt, Z : iCenter ~ ivtzcnt
-		else if (m_TerrainXDesc.iCenter < vFinalPos.x && m_TerrainZDesc.iCenter < vFinalPos.z)
-		{
-			m_TerrainXDesc.iStart = m_TerrainXDesc.iCenter;
-			m_TerrainXDesc.iEnd = m_TerrainXDesc.iEnd;
-			m_TerrainZDesc.iStart = m_TerrainZDesc.iCenter;
-			m_TerrainZDesc.iEnd = m_TerrainZDesc.iEnd;;
-		}
-
-		return _float3(vFinalPos.x, vFinalPos.y, vFinalPos.z);
-	}
+	}	
 
 	return _float3(-1.f, -1.f, -1.f);
 }
@@ -247,29 +107,6 @@ HRESULT CObject_Window::Create_Dummy()
 	m_pDummy->Add_Shader_Component(TEXT("Prototype_Component_Shader_VtxMesh")); ENDINSTANCE;
 
 	return S_OK;
-}
-
-void CObject_Window::InitializeQuadTerrain()
-{
-	// 값 초기화
-	BEGININSTANCE; CVIBuffer_Terrain* pTerrain = static_cast<CVIBuffer_Terrain*>(
-		static_cast<CTerrain*>(pGameInstance->Find_GameObject_In_Layer(LEVEL_TOOL, TEXT("Layer_Tool"),
-			TEXT("GameObject_Terrain")))->Get_Buffer()); ENDINSTANCE;
-
-	_uint ivtxcntX = pTerrain->Get_TerrainSizeX() - 1;
-	_uint ivtxcntZ = pTerrain->Get_TerrainSizeZ() - 1;
-
-	m_TerrainXDesc.iOriginStart = 0;
-	m_TerrainXDesc.iOriginEnd = ivtxcntX;
-	m_TerrainXDesc.iStart = 0;
-	m_TerrainXDesc.iEnd = ivtxcntX;
-	m_TerrainXDesc.iCenter = (m_TerrainXDesc.iOriginStart + m_TerrainXDesc.iOriginEnd) >> 1;
-
-	m_TerrainZDesc.iOriginStart = 0;
-	m_TerrainZDesc.iOriginEnd = ivtxcntZ;
-	m_TerrainZDesc.iStart = 0;
-	m_TerrainZDesc.iEnd = ivtxcntZ;
-	m_TerrainZDesc.iCenter = (m_TerrainZDesc.iOriginStart + m_TerrainZDesc.iOriginEnd) >> 1;
 }
 
 CObject_Window* CObject_Window::Create(ID3D11Device* pDevice, ID3D11DeviceContext* pContext, ImVec2 vWindowPos, ImVec2 vWindowSize)
