2a506d92badf759b6307da0bcc0a9a3bc17c2941
duoface88 <72694443+duoface88@users.noreply.github.com>
2023-09-15 14:12:01 +0900

    Material 중복을 막기 위한 수정
    이제 텍스처들을 한 폴더에 몰아 넣고 중복되는 텍스처가 있다면 하나로 쓸 수 있다.

--- Assimp/Private/Model_Converter.cpp
+++ Assimp/Private/Model_Converter.cpp
@@ -425,29 +425,29 @@ HRESULT CModel_Converter::Convert_Materials(TYPE eType, const char* pModelFilePa
 			CharToWChar(strFileName, szTextureName);
 			lstrcat(szTexturePath, pSaveDirectory);
 
-			/*if (eType != TYPE_MAPOBJECT)
-			{*/
+			if (eType != TYPE_MAPOBJECT)
+			{
 				lstrcat(szTexturePath, szTextureName);
 
 				lstrcpy(Material.MaterialTexture[j].szTexPath, szTexturePath);
 				Material.MaterialTexture[j].TexType = TEXTYPE(j);
-			//}				
-			//else // 맵 오브젝트일 경우 경로를 다르게 설정한다.
-			//{
-			//	wstring wsTexturePath(szTexturePath);
-			//	wstring wsTargetIndex(TEXT("MapObject/"));
-			//	size_t Targetsize = wsTargetIndex.size();
-			//	size_t FindIndex = wsTexturePath.find(wsTargetIndex);
-			//	wstring wsNewTexturePath = wsTexturePath.substr(0, FindIndex + Targetsize);
-			//	wsNewTexturePath += TEXT("Texture/");
+			}
+			else // 맵 오브젝트일 경우 경로를 다르게 설정한다.
+			{
+				wstring wsTexturePath(szTexturePath);
+				wstring wsTargetIndex(TEXT("MapObject/"));
+				size_t Targetsize = wsTargetIndex.size();
+				size_t FindIndex = wsTexturePath.find(wsTargetIndex);
+				wstring wsNewTexturePath = wsTexturePath.substr(0, FindIndex + Targetsize);
+				wsNewTexturePath += TEXT("Texture/");
 
-			//	_tchar	szNewTexturePath[MAX_PATH] = TEXT("");
-			//	lstrcpy(szNewTexturePath, wsNewTexturePath.c_str());
-			//	lstrcat(szNewTexturePath, szTextureName);
+				_tchar	szNewTexturePath[MAX_PATH] = TEXT("");
+				lstrcpy(szNewTexturePath, wsNewTexturePath.c_str());
+				lstrcat(szNewTexturePath, szTextureName);
 
-			//	lstrcpy(Material.MaterialTexture[j].szTexPath, szNewTexturePath);
-			//	Material.MaterialTexture[j].TexType = TEXTYPE(j);
-			//}			
+				lstrcpy(Material.MaterialTexture[j].szTexPath, szNewTexturePath);
+				Material.MaterialTexture[j].TexType = TEXTYPE(j);
+			}			
 		}
 
 		// 머테리얼 저장
