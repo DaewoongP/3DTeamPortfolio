0e0a5eba07b4ad690a998b896848f14cd5b39875
ggumnamu <jyh5101@naver.com>
2023-09-25 17:05:38 +0900

    연동중

--- Client/Private/UI_Effect_Back.cpp
+++ Client/Private/UI_Effect_Back.cpp
@@ -170,6 +170,10 @@ HRESULT CUI_Effect_Back::SetUp_ShaderResources()
 			return E_FAIL;
 	}
 		break;
+	case Client::CUI_Effect_Back::SKILL:
+		if (FAILED(m_pShaderCom->Bind_RawValue("g_isClicked", &m_isClicked, sizeof(_bool))))
+			return E_FAIL;
+		break;
 	}
 
 	return S_OK;
