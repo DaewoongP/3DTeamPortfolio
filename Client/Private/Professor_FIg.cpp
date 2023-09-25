ef7a2a06b7ecfd24f9c299c1114840961a7db509
ggm4035 <110024770+ggm4035@users.noreply.github.com>
2023-09-25 21:55:17 +0900

    스킬 오프셋 조정

--- Client/Private/Professor_FIg.cpp
+++ Client/Private/Professor_FIg.cpp
@@ -942,7 +942,7 @@ void CProfessor_Fig::Attack_Light()
 		return;
 
 	m_pMagicSlot->Action_Magic_Basic(0, m_pTarget->Get_Transform(),
-		XMMatrixIdentity(),
+		XMMatrixTranslation(0.f, 2.5f, 0.f),
 		m_pWeapon->Get_Transform()->Get_WorldMatrixPtr(),
 		m_pWeapon->Get_Wand_Point_OffsetMatrix(),
 		COL_ENEMY);
@@ -954,7 +954,7 @@ void CProfessor_Fig::Attack_Heavy()
 		return;
 
 	m_pMagicSlot->Action_Magic_Basic(0, m_pTarget->Get_Transform(),
-		XMMatrixIdentity(),
+		XMMatrixTranslation(0.f, 2.5f, 0.f),
 		m_pWeapon->Get_Transform()->Get_WorldMatrixPtr(),
 		m_pWeapon->Get_Wand_Point_OffsetMatrix(),
 		COL_ENEMY);
@@ -966,7 +966,7 @@ void CProfessor_Fig::Cast_Levioso()
 		return;
 
 	m_pMagicSlot->Action_Magic_Skill(0, m_pTarget->Get_Transform(),
-		XMMatrixIdentity(),
+		XMMatrixTranslation(0.f, 2.5f, 0.f),
 		m_pWeapon->Get_Transform()->Get_WorldMatrixPtr(),
 		m_pWeapon->Get_Wand_Point_OffsetMatrix(),
 		COL_ENEMY);
@@ -978,7 +978,7 @@ void CProfessor_Fig::Cast_Protego()
 		return;
 
 	m_pMagicSlot->Action_Magic_Basic(1, m_pTransform,
-		XMMatrixIdentity(),
+		XMMatrixTranslation(0.f, 1.f, 0.f),
 		m_pWeapon->Get_Transform()->Get_WorldMatrixPtr(),
 		m_pWeapon->Get_Wand_Point_OffsetMatrix(),
 		COL_MAGIC);
