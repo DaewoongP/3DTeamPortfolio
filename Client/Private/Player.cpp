f37d9af6facaa1ea1209d6bc7703cb48d21f9fd6
AnCheolMin <94519819+AnCheolMin@users.noreply.github.com>
2023-10-06 11:40:22 +0900

    디핀도 이펙트 보강 봄바르다 제작

--- Client/Private/Player.cpp
+++ Client/Private/Player.cpp
@@ -856,12 +856,25 @@ HRESULT CPlayer::Add_Magic()
 		m_pMagicSlot->Add_Magics(magicInitDesc);
 	}
 
+	//º½¹Ù¸£´Ù
+	{
+		magicInitDesc.eBuffType = BUFF_NONE;
+		magicInitDesc.eMagicGroup = CMagic::MG_ESSENTIAL;
+		magicInitDesc.eMagicType = CMagic::MT_NOTHING;
+		magicInitDesc.eMagicTag = BOMBARDA;
+		magicInitDesc.fInitCoolTime = 1.f;
+		magicInitDesc.iDamage = 10;
+		magicInitDesc.isChase = true;
+		magicInitDesc.fLifeTime = 0.8f;
+		m_pMagicSlot->Add_Magics(magicInitDesc);
+	}
+
 	m_pMagicSlot->Add_Magic_To_Basic_Slot(2, LUMOS);
 	m_pMagicSlot->Add_Magic_To_Basic_Slot(3, FINISHER);
 
-	Set_Spell_Botton(0, DESCENDO);
-	Set_Spell_Botton(1, FLIPENDO);
-	Set_Spell_Botton(2, EXPELLIARMUS);
+	Set_Spell_Botton(0, LEVIOSO);
+	Set_Spell_Botton(1, DIFFINDO);
+	Set_Spell_Botton(2, BOMBARDA);
 	Set_Spell_Botton(3, IMPERIO);
 
 	return S_OK;
