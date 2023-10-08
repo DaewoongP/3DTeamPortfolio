5876b524b85f5d9af122ef49d71961f6c5ba21cc
AnCheolMin <94519819+AnCheolMin@users.noreply.github.com>
2023-10-08 15:59:23 +0900

    평타 디스토션 쉴드 터지기 (#233)

--- Client/Private/Player.cpp
+++ Client/Private/Player.cpp
@@ -1597,7 +1597,7 @@ void CPlayer::Shot_Basic_Last_Spell()
 
 void CPlayer::Protego()
 {
-	m_pMagicSlot->Action_Magic_Basic(1, this, m_pWeapon, (COLLISIONFLAG)(COL_ENEMY | COL_ENEMY_ATTACK), m_isPowerUp);
+	m_pMagicSlot->Action_Magic_Basic(1, this, m_pWeapon, (COLLISIONFLAG)(COL_ENEMY | COL_ENEMY_ATTACK|COL_MAGIC), m_isPowerUp);
 }
 
 void CPlayer::Gravity_On()
