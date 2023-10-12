4dc825230f749f89f52ceb58106a593ba3b73378
duoface88 <72694443+duoface88@users.noreply.github.com>
2023-10-12 21:25:04 +0900

    늑대 주석 처리 2
    내용 무

--- Client/Private/Main2_Loader.cpp
+++ Client/Private/Main2_Loader.cpp
@@ -194,16 +194,16 @@ HRESULT CMain2_Loader::Loading_For_Cliffside(LEVELID eLevelID)
 			CDarkWizard_M::Create(m_pDevice, m_pContext))))
 			throw TEXT("Prototype_GameObject_DarkWizard_M");
 
-		PivotMatrix = XMMatrixScaling(0.25f, 0.25f, 0.25f);
-		/* For.Prototype_Component_Model_Wolf */
-		if (FAILED(m_pGameInstance->Add_Prototype(eLevelID, TEXT("Prototype_Component_Model_Wolf"),
-			CModel::Create(m_pDevice, m_pContext, CModel::TYPE_ANIM, TEXT("../../Resources/Models/Anims/Wolf/Wolf.gcm"), PivotMatrix))))
-			throw TEXT("Prototype_Component_Model_Wolf");
+		//PivotMatrix = XMMatrixScaling(0.25f, 0.25f, 0.25f);
+		///* For.Prototype_Component_Model_Wolf */
+		//if (FAILED(m_pGameInstance->Add_Prototype(eLevelID, TEXT("Prototype_Component_Model_Wolf"),
+		//	CModel::Create(m_pDevice, m_pContext, CModel::TYPE_ANIM, TEXT("../../Resources/Models/Anims/Wolf/Wolf.gcm"), PivotMatrix))))
+		//	throw TEXT("Prototype_Component_Model_Wolf");
 
-		/* For.Prototype_GameObject_Wolf */
-		if (FAILED(m_pGameInstance->Add_Prototype(eLevelID, TEXT("Prototype_GameObject_Wolf"),
-			CWolf::Create(m_pDevice, m_pContext))))
-			throw TEXT("Prototype_GameObject_Wolf");
+		///* For.Prototype_GameObject_Wolf */
+		//if (FAILED(m_pGameInstance->Add_Prototype(eLevelID, TEXT("Prototype_GameObject_Wolf"),
+		//	CWolf::Create(m_pDevice, m_pContext))))
+		//	throw TEXT("Prototype_GameObject_Wolf");
 
 		/* =========================================================== */
 	}
