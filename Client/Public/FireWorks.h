43ced2141a13e08f640cb0801b1e0f3f884583c6
shortusernam <wjdghks718@gmail.com>
2023-10-27 11:17:15 +0900

    fireworks

---
+++ Client/Public/FireWorks.h
@@ -0,0 +1,61 @@
+#include "MapObject.h"
+#include "Client_Defines.h"
+
+BEGIN(Engine)
+class CMesh;
+class CModel;
+class CShader;
+class CRenderer;
+class CParticleSystem;
+class CLight;
+END
+
+BEGIN(Client)
+
+class CFireWorks : public CMapObject
+{
+public:
+	typedef struct tagMapObjectDesc
+	{
+		_float4x4 WorldMatrix; // 상태 행렬
+		_uint iTagLen = { 0 }; // 문자열 길이
+		_tchar wszTag[MAX_PATH] = TEXT(""); // 오브젝트 종류(모델 컴포넌트 이름)
+	}MAPOBJECTDESC;
+
+protected:
+	explicit CFireWorks(ID3D11Device* pDevice, ID3D11DeviceContext* pContext);
+	explicit CFireWorks(const CFireWorks& rhs);
+	virtual ~CFireWorks() = default;
+
+public:
+	virtual HRESULT Initialize_Prototype();
+	virtual HRESULT Initialize(void* pArg) override;
+	virtual HRESULT Initialize_Level(_uint iCurrentLevelIndex) override;
+	virtual void Tick(_float fTimeDelta) override;
+	virtual void Late_Tick(_float fTimeDelta) override;
+	virtual HRESULT Render() override;
+
+private:
+	CShader* m_pShader = { nullptr };
+	CShader* m_pShadowShader = { nullptr };
+	CRenderer* m_pRenderer = { nullptr };
+	CModel* m_pModel = { nullptr };
+	vector<CParticleSystem*> m_vecParticle;
+private:
+	// 절두체 컬링을 위해 Bounding Box를 생성 하기위한 최소, 최대 정점
+	_float3			m_vMinPoint, m_vMaxPoint, m_vCenterPoint;
+	_float			m_fRadius = { 0.f };
+	MAPOBJECTDESC	m_ObjectDesc;
+
+	_float4			m_vEmissive;
+
+private:
+	HRESULT Add_Components();
+	
+
+public:
+	static CFireWorks* Create(ID3D11Device* pDevice, ID3D11DeviceContext* pContext);
+	virtual CGameObject* Clone(void* pArg) override;
+	virtual void Free() override;
+};
+END
